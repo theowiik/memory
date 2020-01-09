eb1ee4d1312e53c0232a1d4bafc5089b4bdd8d2e
Theo Wiik <theo.le.wiik@gmail.com>
2020-01-09 20:22:56 +0100

    generate random pattern and display the pattern

--- memory.ino
+++ memory.ino
@@ -1,39 +1,98 @@
-// A simple memory game.
+// A simple color memory game.
 // Repeat the color pattern that is dsplayed.
 
 // The length of the pattern to win with
 const int winLength = 10;
 
+// remove and retrieve from the size of tthe LIGHT_OUTPUTS arr.
 const int N_LAMPS = 3;
+
+// Out
 const int LIGHT_ONE_OUT = 13;
-const int LIGHT_ONE_IN = 2;
+const int LIGHT_TWO_OUT = 12;
+const int LIGHT_THREE_OUT = 11;
+
+const int LIGHT_OUTPUTS[N_LAMPS] = {LIGHT_ONE_OUT, LIGHT_TWO_OUT, LIGHT_THREE_OUT};
+
+// Input
+const int LIGHT_ONE_IN = 4;
+const int LIGHT_TWO_IN = 3;
+const int LIGHT_THREE_IN = 2;
 
 int pattern[winLength];
 int level;
 
 void setup() {
-  level = 1;
+  level = 6;
   generatePattern(pattern);
 
+  // Out
   pinMode(LIGHT_ONE_OUT, OUTPUT);
-  pinMode(LIGHT_ONE_IN, INPUT);
+  pinMode(LIGHT_TWO_OUT, OUTPUT);
+  pinMode(LIGHT_THREE_OUT, OUTPUT);
 
-  // debug light
-  pinMode(8, OUTPUT);
+  // In
+  pinMode(LIGHT_ONE_IN, INPUT);
+  pinMode(LIGHT_TWO_IN, INPUT);
+  pinMode(LIGHT_THREE_IN, INPUT);
 }
 
 void loop() {
-  //digitalWrite(LIGHT_ONE_OUT, HIGH);
+  displayStart();
+  displayPattern();
+}
 
-  if (digitalRead(LIGHT_ONE_IN) == HIGH) {
-    digitalWrite(8, HIGH);
-  } else {
-    digitalWrite(8, LOW);
+/**
+ * Displays the start of the pattern.
+ */
+void displayStart() {
+  int sleepTime = 1000;
+
+  allLightsOff();
+  delay(sleepTime);
+  allLightsOn();
+  delay(sleepTime);
+  allLightsOff();
+  delay(sleepTime);
+}
+
+void allLightsOff() {
+  setAllLightsTo(LOW);
+}
+
+void allLightsOn() {
+  setAllLightsTo(HIGH);
+}
+
+void setAllLightsTo(int lightStatus) {
+  digitalWrite(LIGHT_ONE_OUT, lightStatus);
+  digitalWrite(LIGHT_TWO_OUT, lightStatus);
+  digitalWrite(LIGHT_THREE_OUT, lightStatus);
+}
+
+/**
+ * Displays the pattern.
+ */
+void displayPattern() {
+  int sleepTime = 500;
+  for (int i = 0; i < level; i++) {
+    digitalWrite(pattern[i], HIGH);
+    delay(sleepTime);
+    digitalWrite(pattern[i], LOW);
+    delay(sleepTime);
   }
 }
 
+/**
+ * Generates the pattern to repeat.
+ * @arr[] the array populate.
+ */
 void generatePattern(int arr[]) {
+  randomSeed(analogRead(0));
+
   for (int i = 0; i < winLength; i++) {
-    arr[i] = random(N_LAMPS);
+    int arraySize = (sizeof(LIGHT_OUTPUTS) / sizeof(int));
+    int index = random(3);
+    arr[i] = LIGHT_OUTPUTS[index];
   }
 }
